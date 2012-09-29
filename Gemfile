1c0f6edef8c620d5441df88ef4823a8e5e67ba45
Sam Serpoosh <ssjesus287@gmail.com>
2012-09-30 00:10:15 +0330

    initiated transaction app with Gemfile

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'webrat', '0.7.2'
+end
+
+gem 'jquery-rails'
+gem 'therubyracer'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransactionTracker::Application.config.secret_token = 'a4753804d52ba790d885722993f94068471b34e8e348d7ce2299e5f533f05b056383340d8772c33673969bd11153dd097023250fdd2f137ab80ed17c63e302b1'
